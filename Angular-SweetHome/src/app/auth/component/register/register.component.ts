1c37906a3b8d7753c80de142e0ead7f0d4b3e102
kibo <minhtuanvt89@gmail.com>
2021-07-29 00:05:05 +0700

    auth

--- Angular-SweetHome/src/app/auth/component/register/register.component.ts
+++ Angular-SweetHome/src/app/auth/component/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../../shared/model/user";
+import {Router} from "@angular/router";
+import {AuthService} from "../../service/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,128 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  formRegister !: FormGroup;
+  message:string|undefined;
+  userModelObj: User = new class implements User {
+    address ?: string;
+    email ?: string;
+    fullName ?: string;
+    id ?: number;
+    image ?: string;
+    name ?: string;
+    password ?: string;
+    phone ?: string;
+    token ?: string;
+  };
 
-  constructor() { }
+  error_messages = {
+    'name': [
+      {type: 'required', message: 'User Name is required.'},
+      {type: 'minlength', message: 'User Name min length.'},
+      {type: 'maxlength', message: 'User Name max length.'},
+    ],
+    'email': [
+      {type: 'required', message: 'Email is required.'},
+      {type: 'email', message: 'Email  wrong!'},
+    ],
+    'phone': [
+      {type: 'required', message: 'Phone is required.'},
+      {type: 'pattern', message: 'Telephone number wrong!'},
+    ],
+    'password': [
+      {type: 'required', message: 'Password is required.'},
+      {type: 'minlength', message: 'Password min length.'},
+      {type: 'maxlength', message: 'Password max length.'},
+    ],
+    'confirmPassword': [
+      {type: 'required', message: 'Confirm Password is required.'},
+      {type: 'minlength', message: 'Confirm Password min length.'},
+      {type: 'maxlength', message: 'Confirm Password max length.'},
+    ],
+  }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    public authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.formRegister = this.fb.group({
+      name: new FormControl ('', [
+        Validators.required,
+        Validators.minLength(4)]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern('(84|0[3|5|7|8|9])+([0-9]{8})'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)]),
+      confirmPassword: new FormControl('',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+    },{
+      validators: this.passwordMatch.bind(this)
+    })
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.formRegister.controls; }
+
+  submitRegister():void {
+
+    this.userModelObj.name = this.formRegister.value.name;
+    this.userModelObj.password = this.formRegister.value.password;
+    // @ts-ignore
+    this.userModelObj.password_confirmation = this.formRegister.value.confirmPassword;
+    this.userModelObj.email = this.formRegister.value.email;
+
+    this.authService.createUser(this.userModelObj).subscribe(res =>{
+        this.message = res.message
+        console.log(this.message)
+        let ref = document.getElementById('cancel')
+        ref?.click();
+        this.formRegister.reset();
+        this.router.navigate(['../login']);
+      },
+      error => {
+        this.message = error.error
+      })
+  }
+
+  passwordMatch(formGroup: FormGroup) {
+    // @ts-ignore
+    const {value: password} = formGroup.get('password');
+    // @ts-ignore
+    const {value: confirmPassword} = formGroup.get('confirmPassword');
+    return password === confirmPassword ? null : {passwordNotMatch: true};
+  }
+
+  get name(){
+    return this.formRegister?.get('name')
+  }
+
+  get email(){
+    return this.formRegister?.get('email')
+  }
+
+  get phone(){
+    return this.formRegister?.get('phone')
+  }
+
+  get password(){
+    return this.formRegister?.get('password')
+  }
+  get confirmPassword(){
+    return this.formRegister?.get('confirmPassword')
+  }
+
 
 }
