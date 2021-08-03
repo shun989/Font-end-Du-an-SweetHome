4ffef6858de25c9e257d6a25f18d37cf30ee38c5
aud1ence <daruma19051890@gmail.com>
2021-08-03 15:07:49 +0700

    create booking template

--- Angular-SweetHome/src/app/home-page/component/detail/detail.component.ts
+++ Angular-SweetHome/src/app/home-page/component/detail/detail.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {ApartmentService} from "../../../service/apartment.service";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormArray, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../../auth/service/auth.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-detail',
@@ -11,26 +13,71 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class DetailComponent implements OnInit {
   apartment: any;
   category: any;
+  isLogin: boolean | undefined;
+  user: any;
+  unavailabilityForm!: FormGroup;
+  unavailability = {startDate: '', endDate: ''}
 
   constructor(private route: ActivatedRoute,
-              private apartmentService: ApartmentService) {
+              private apartmentService: ApartmentService,
+              public authService: AuthService,
+              private router: Router,
+              private toastr: ToastrService,
+              private formBuilder: FormBuilder) {
+    this.unavailabilityForm = this.formBuilder.group({
+      startDate: [this.unavailability.startDate],
+      endDate: [this.unavailability.endDate]
+    });
+    this.unavailabilityForm = this.formBuilder.group({
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+    })
   }
 
   ngOnInit(): void {
     // @ts-ignore
     let id = +this.route.snapshot.paramMap.get('id');
     this.getById(id)
+    this.authService.currentLogin.subscribe(isLogin => this.isLogin = isLogin);
+    this.authService.currentUserLogin.subscribe(user => this.user = user);
+
+  }
+
+  submit() {
+    let data = this.unavailabilityForm?.value;
+    let start = new Date(data.startDate);
+    let end = new Date(data.endDate);
+
+    console.log(end.getDay());
   }
 
   getById(id: number) {
     this.apartmentService.getById(id).subscribe((data) => {
       this.apartment = data
-      console.log(data)
     });
   }
 
-  range = new FormGroup({
-    start: new FormControl(),
-    end: new FormControl()
-  });
+  getErrorMessageStartDate() {
+    if (this.startDate?.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.startDate?.hasError('startDate') ? 'Not a valid start date' : '';
+
+  }
+
+  getErrorMessageEndDate() {
+    if (this.endDate?.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.endDate?.hasError('endDate') ? 'Not a valid end date' : '';
+
+  }
+
+  get startDate() {
+    return this.unavailabilityForm?.get('startDate')
+  }
+
+  get endDate() {
+    return this.unavailabilityForm?.get('endDate')
+  }
 }
