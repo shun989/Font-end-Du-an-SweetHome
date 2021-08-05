db8b9e496a5637360d0d932482fb3994c82625ff
aud1ence <daruma19051890@gmail.com>
2021-08-05 17:26:29 +0700

    done cancel booking

--- Angular-SweetHome/src/app/home-action/component/bookmarked-list/bookmarked-list.component.ts
+++ Angular-SweetHome/src/app/home-action/component/bookmarked-list/bookmarked-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {BookingService} from "../../../service/booking.service";
 import {ActivatedRoute} from "@angular/router";
 import {Apartment} from "../../../shared/model/apartment";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-bookmarked-list',
@@ -10,15 +11,32 @@ import {Apartment} from "../../../shared/model/apartment";
 })
 export class BookmarkedListComponent implements OnInit {
   apartments: any;
+  booking_id!: number;
+  message!: string;
 
   constructor(private bookingService: BookingService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private toastr: ToastrService,) {
   }
 
   ngOnInit(): void {
     // @ts-ignore
     let id = +this.route.snapshot.paramMap.get('id');
     this.getBookmark(id);
+  }
+
+  changeBookingId(id: any) {
+    this.booking_id = id;
+    console.log(this.booking_id)
+  }
+
+  submitBookingDel() {
+    this.bookingService.deleteBookmarked(this.booking_id).subscribe((res) => {
+      this.message = res.message
+      this.toastr.success('Thank you', this.message)
+    }, error => {
+      this.toastr.error('Sorry', 'Bạn không được hủy trước một ngày check in')
+    })
   }
 
   getBookmark(id: number) {
